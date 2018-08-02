929965b0bc2f5584bcb2978a8cc5a4716006d6ed
brysona10 <addisonbryson10@gmail.com>
2018-08-02 16:03:37 +0000

    working v

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,30 @@ class ApplicationController < Sinatra::Base
   get "/" do
   	erb :index
   end
+  
+  post "/results" do
+    bn = params["price"] + params["style"]
+    brands = {"1t"=>"Forever21","1c"=>"old_navy","1p"=>"HM","2t"=>"Zara","2c"=>"Nike_Adidas","2t"=>"J_Crew","3t"=>"Urban_outfitters","3c"=>"lulu","3p"=>"Ralph_Lauren"}
+    if bn == "1t"
+      erb :forever21
+    elsif bn == "1c"
+      erb :oldnavy
+    elsif bn == "1p"
+      erb :hm
+    elsif bn == "2t"
+      erb :zara
+    elsif bn == "2c"
+      erb :nikeadidas
+    elsif bn == "2p"
+      erb :jcrew
+    elsif bn == "3t"
+      erb :urban
+    elsif bn =="3c"
+      erb :lulu
+    elsif bn == "3p"
+      erb :ralph
+    else 
+      "Sorry We Could Not Find A Brand For You"
+    end
+  end
 end
